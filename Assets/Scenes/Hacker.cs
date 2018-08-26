1dc689ff9202c4972ac3077d2a329a19ef765fee
Conor Ward <conorward509@hotmail.com>
2018-08-26 15:01:54 +0100

    Terminal Hacker First Commit

---
+++ Assets/Scenes/Hacker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        showMainMenu();
+	}
+
+    void showMainMenu()
+    {
+        Terminal.WriteLine("Select from one of the following to hack into.");
+        Terminal.WriteLine(" ");
+        Terminal.WriteLine(" 1. State Exams Commission ");
+        Terminal.WriteLine(" 2. Facebook ");
+        Terminal.WriteLine(" 3. The Pentagon ");
+
+        Terminal.WriteLine(" Enter Selection: ");
+     }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
