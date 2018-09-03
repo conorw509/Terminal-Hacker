3c6b42fece7c98a50f45e1a9904bbd91a07599d3
Conor Ward <conorward509@hotmail.com>
2018-09-03 20:01:26 +0100

    state and enum types

--- Assets/Scenes/Hacker.cs
+++ Assets/Scenes/Hacker.cs
@@ -3,42 +3,86 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Hacker : MonoBehaviour {
+    
+    //Game State
+    int level;
+    enum Screen { MainMenu, PasswordScreen, WinScreen}
+    Screen currentScreen;
 
-	// Use this for initialization
+	
+    // Use this for initialization
 	void Start () {
-
-        showMainMenu();  
+       showMainMenu();  
 	}
 
     void showMainMenu()
     {
-
+        currentScreen = Screen.MainMenu;
         Terminal.WriteLine("Select from one of the following to hack into.");
         Terminal.WriteLine(" ");
         Terminal.WriteLine(" 1. State Exams Commission ");
         Terminal.WriteLine(" 2. Facebook ");
         Terminal.WriteLine(" 3. The Pentagon ");
-
         Terminal.WriteLine(" Enter Selection: ");
      }
 
     void OnUserInput(string input)
     {
-        //Terminal.WriteLine (" User entered: " + input);
+
         if (input == "menu")
         {
-            Terminal.ClearScreen();
-             showMainMenu();
-        }
-        else if (input == "Conor")
-        {
-            Terminal.WriteLine("Welcome Master Conor");
+         
+            showMainMenu();
+            
         }
 
-        else
+        else if (currentScreen == Screen.MainMenu)
         {
-            Terminal.WriteLine("Select a valid input");
+            RunMainMenu(input);
         }
+    }
+
+        void RunMainMenu(string userInput) { 
+        {
+
+            if (userInput == "1")
+            {
+
+                level = 1;
+                startGame();
+            }
+
+            else if (userInput == "2")
+            {
+
+                level = 2;
+                startGame();
+            }
+
+            else if (userInput == "3")
+            {
+
+                level = 3;
+                startGame();
+            }
+            else if (userInput == "Conor")
+            {
+                Terminal.WriteLine("Welcome Master Conor");
+            }
+            else
+            {
+                Terminal.WriteLine("Select a valid input");
+            }
+        }
+    }
+
+    void startGame()
+    {
+        currentScreen = Screen.PasswordScreen;
+        Terminal.WriteLine(" You have chosen level: " +level);
+        Terminal.WriteLine(" Please Enter a Password: ");
+        
+
     }
     
 	
